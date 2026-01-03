5532beabc0a601f2c6e2109ddd88df8435289da3
Talha Zeren <talhazeren97@gmail.com>
2026-01-03 15:49:08 +0300

    Branch name has been changed and added a new class into cards

--- cards/src/main/java/com/eazybytes/cards/entity/Cards.java
+++ cards/src/main/java/com/eazybytes/cards/entity/Cards.java
@@ -1,20 +1,34 @@
 package com.eazybytes.cards.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-
-
+@Entity
+@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cards {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardId;
+
+    private String mobileNumber;
+
+    private String cardNumber;
+
+    private String cardType;
+
+    private int totalLimit;
+
+    private int amountUsed;
+
+    private int availableAmount;
 
 
 }
